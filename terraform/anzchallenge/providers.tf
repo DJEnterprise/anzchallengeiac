5282d5eb0f0e1710732da1044543e5c5dc012dc5
Dhanaraj Kumar <dhan@Dhanarajs-MacBook-Pro.local>
2021-11-17 22:19:16 +1100

    adding version for provider

--- terraform/anzchallenge/providers.tf
+++ terraform/anzchallenge/providers.tf
@@ -2,17 +2,12 @@ terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            versoin = "~> 3.2"
+            versoin = "~> 3.20.0"
         }
 
-        random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+      random = {
+          source  = "hashicorp/random"
+          version = "3.1.0"
+       }
     }
-    }
-}
-
-provider "aws" {
-    region = var.region
-   
 }
