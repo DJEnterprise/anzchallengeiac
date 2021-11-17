7f065a86cfed0e6534bc3a3bcebcf66a5784368c
Dhanaraj Kumar <dhan@Dhanarajs-MacBook-Pro.local>
2021-11-18 03:19:14 +1100

    Adding Nodes

--- terraform/anzchallenge/main.tf
+++ terraform/anzchallenge/main.tf
@@ -79,6 +79,67 @@ resource "aws_eks_fargate_profile" "anzchallenge-eks-fargate-profile" {
   depends_on            = [module.eks]
 }
 
+
+resource "aws_iam_role" "anzchallengenoderole" {
+  name = "eks-node-group-anzchallengenoderole"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+   depends_on            = [aws_eks_fargate_profile.anzchallenge-eks-fargate-profile]
+}
+
+resource "aws_iam_role_policy_attachment" "anzchallengenoderole-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.anzchallengenoderole.name
+  depends_on = [aws_iam_role.anzchallengenoderole]
+}
+
+resource "aws_iam_role_policy_attachment" "anzchallenge-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.anzchallengenoderole.name
+  depends_on = [aws_iam_role.anzchallengenoderole]
+}
+
+resource "aws_iam_role_policy_attachment" "anzchallenge-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.anzchallengenoderole.name
+  depends_on = [aws_iam_role.anzchallengenoderole]
+}
+
+
+resource "aws_eks_node_group" "anzchallengenodes" {
+  cluster_name    = local.cluster_name
+  node_group_name = "anzchallengenodes"
+  node_role_arn   = aws_iam_role.anzchallengenoderole.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 2
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.anzchallengenoderole-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.anzchallengenoderole-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.anzchallengenoderole-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
 resource "kubernetes_pod" "anzchallengepod" {
   metadata {
     name = "anzchallenge-app"
@@ -96,7 +157,7 @@ resource "kubernetes_pod" "anzchallengepod" {
       }
       }
     }
-    depends_on  = [aws_eks_fargate_profile.anzchallenge-eks-fargate-profile]
+    depends_on  = [aws_eks_node_group.anzchallengenodes]
 }
 
 resource "kubernetes_service" "anzchallenge-service" {
