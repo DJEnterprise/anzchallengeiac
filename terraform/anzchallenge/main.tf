c10b94a719c274df59caf20db1a6d1a956182cdc
Dhanaraj Kumar <dhan@Dhanarajs-MacBook-Pro.local>
2021-11-18 00:22:00 +1100

    Adding more changes

--- terraform/anzchallenge/main.tf
+++ terraform/anzchallenge/main.tf
@@ -31,3 +31,93 @@ module "vpc" {
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "anz_challenge_sg2 {
+  name_prefix = "anz_challenge_sg2"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "anz_challenge_sg1" {
+  name_prefix = "anz_challenge_sg1"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.20"
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    name = "anzchallenge"
+ 
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "anzchallenge-group-1"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo anzchallenge group 1"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.anz_challenge_sg1.id]
+    },
+    {
+      name                          = "anzchallenge-group-2"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo anzchallenge group 2"
+      additional_security_group_ids = [aws_security_group.anz_challenge_sg2.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
