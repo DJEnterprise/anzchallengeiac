419c69d3834d996285da9fc35e3e7b2228b2fcc4
Dhanaraj Kumar <dhan@Dhanarajs-MacBook-Pro.local>
2021-11-18 00:30:12 +1100

    Adding version

--- terraform/anzchallenge/providers.tf
+++ terraform/anzchallenge/providers.tf
@@ -5,10 +5,10 @@ terraform {
             version = "~> 3.20.0"
         }
 
-      random = {
-          source  = "hashicorp/random"
-          version = "3.1.0"
-       }
+      kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
     }
 }
 
