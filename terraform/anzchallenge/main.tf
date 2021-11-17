5356345722842d91d275635cefbeb838fd9e3ba0
Dhanaraj Kumar <dhan@Dhanarajs-MacBook-Pro.local>
2021-11-17 23:17:43 +1100

    fixing cluster

--- terraform/anzchallenge/main.tf
+++ terraform/anzchallenge/main.tf
@@ -1,12 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "anzchallenge-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  cluster_name = "anzchallenge-eks-cluster"
 }
 
 module "vpc" {
